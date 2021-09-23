f860ea864e641739406e50588c83370d65954423
Omar <omar@notarize.com>
2021-09-22 19:19:29 -0700

    binary approach for deps

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ReactiveDogSPM",
+    name: "ReactiveDog",
     platforms: [
         .iOS(.v13)
     ],
@@ -13,18 +13,15 @@ let package = Package(
         .library(
             name: "ReactiveDog",
             targets: ["ReactiveDog"]),
-        .library(name: "ReactiveDogDeps", targets: ["ReactiveDogDeps"])
+        .library(name: "RxSwift", targets: ["RxSwift"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift", .exact("6.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ReactiveDog", path: "Sources/ReactiveDog.xcframework"),
-        .target(name: "ReactiveDogDeps", dependencies: [
-            "RxSwift"
-        ])
+        .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework.zip", checksum: "288f199ba6ad37fbf4155609e3be33296ccea4d95ba3b556459ab7f7c9ad396f"),
+        .binaryTarget(name: "RxSwift", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxSwift.xcframework.zip", checksum: "411c88b4b491daf01786c30569f6f9a5fdeb37911a86b2561b253fa4e744e4f3")
     ]
 )
