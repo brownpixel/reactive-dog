9aa9a0fd9cfff15a714171878a1a88ae9200fb99
Omar <omar@notarize.com>
2021-09-23 19:51:02 -0700

    reactivedog 3

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework-2.zip", checksum: "2b0df25f4333bac52cc6299b1e90239662e1b22f07b3adffabff0582312f5885"),
+        .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework-3.zip", checksum: "57a39d4e453d6cbebd4b18c7ccba3b946fb52aa97e24c2c662b9b26d61874999"),
         .target(name: "ReactivDogDeps", dependencies: [
             "RxSwift"
         ])
