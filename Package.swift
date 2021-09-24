ead10a77438af40114177a623b602bb9f76e651c
Omar <omar@notarize.com>
2021-09-23 19:32:28 -0700

    dependencies via deps target

--- Package.swift
+++ Package.swift
@@ -12,13 +12,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ReactiveDog",
-            targets: ["ReactiveDog"])
+            targets: ["ReactiveDog"]),
+        .library(name: "ReactivDogDeps", targets: ["ReactivDogDeps"])
     ],
     dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", .exact("6.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework-2.zip", checksum: "2b0df25f4333bac52cc6299b1e90239662e1b22f07b3adffabff0582312f5885"),
+        .target(name: "ReactivDogDeps", dependencies: [
+            "RxSwift"
+        ])
     ]
 )
