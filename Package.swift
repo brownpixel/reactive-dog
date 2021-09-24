9db2ca583717b12e00874a3742f279b34c01e284
Omar <omar@notarize.com>
2021-09-23 19:24:26 -0700

    update without rxswift binary

--- Package.swift
+++ Package.swift
@@ -12,16 +12,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ReactiveDog",
-            targets: ["ReactiveDog"]),
-        .library(name: "RxSwift", targets: ["RxSwift"])
+            targets: ["ReactiveDog"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework.zip", checksum: "288f199ba6ad37fbf4155609e3be33296ccea4d95ba3b556459ab7f7c9ad396f"),
-        .binaryTarget(name: "RxSwift", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/RxSwift.xcframework.zip", checksum: "411c88b4b491daf01786c30569f6f9a5fdeb37911a86b2561b253fa4e744e4f3")
+        .binaryTarget(name: "ReactiveDog", url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/ReactiveDog.xcframework-2.zip", checksum: "2b0df25f4333bac52cc6299b1e90239662e1b22f07b3adffabff0582312f5885"),
     ]
 )
